614c5bb40449f1cbc78cbe779b3046c66709d9b5
hardy_hastings <157944600+levidoc@users.noreply.github.com>
2024-12-26 07:01:28 +0200

    Update link.php

--- bin/link.php
+++ bin/link.php
@@ -1,3 +1,6 @@
 <?php 
+include_once "database.php";           #This will contain the database Scripts 
+include_once "external_source.php";    #This will contain all the external API data communication Scripts  
 
+#This file will be used to link all the external Services Together 
 ?>
